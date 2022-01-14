300e34d51ce76c2948790c1c90e0db9d1b1207a1
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-01-14 08:01:04 +0100

    Add project files.

---
+++ CampSleepaway.Domain/Counselor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampSleepaway.Domain
+{
+    public class Counselor : Person
+    {
+        public string PhoneNumber { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ CampSleepawayUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CampSleepawayUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
