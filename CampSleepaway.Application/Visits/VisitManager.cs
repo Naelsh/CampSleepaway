16bb330000d20048a38604b13a5a98530722efd1
Niklas Lindblad <niklas.o.lindblad@gmail.com>
2022-01-15 14:58:09 +0100

    once again started with visit :P

---
+++ CampSleepaway.Application/Visits/VisitManager.cs
@@ -0,0 +1,26 @@
+﻿//using CampSleepaway.Domain.Data;
+//using CampSleepaway.Persistence;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CampSleepaway.Application.Visits
+//{
+//    class VisitManager : ManagerCore
+//    {
+//        public VisitManager(CampSleepawayContext context) : base(context)
+//        {
+//        }
+
+//        public int CreateVisit(
+//            Camper camper,
+//            DateTime startTime,
+//            DateTime endTime,
+//            params NextOfKin[] nextOfKin)
+//        {
+//            return 0;
+//        }
+//    }
+//}
