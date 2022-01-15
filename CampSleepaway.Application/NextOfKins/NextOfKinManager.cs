f0b4dd19f2a58ad82ca92e93a63ea99dcdebb158
Niklas Lindblad <niklas.o.lindblad@gmail.com>
2022-01-15 14:36:49 +0100

    creation of nextofkin added

--- CampSleepaway.Application/NextOfKins/NextOfKinManager.cs
+++ CampSleepaway.Application/NextOfKins/NextOfKinManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CampSleepaway.Domain.Data;
+using CampSleepaway.Persistence;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,21 @@ using System.Threading.Tasks;
 
 namespace CampSleepaway.Application.NextOfKins
 {
-    class NextOfKinManager
+    public class NextOfKinManager : ManagerCore
     {
+        public NextOfKinManager(CampSleepawayContext context) : base(context)
+        {
+        }
+
+        public int AddNextOfKin(NextOfKin nextOfKin)
+        {
+            if (nextOfKin.FirstName == null) { return 0; }
+            if (nextOfKin.LastName == null) { return 0; }
+            if (nextOfKin.Children.Count == 0) { return 0; }
+
+            _context.NextOfKins.Add(nextOfKin);
+            var result = _context.SaveChanges();
+            return result;
+        }
     }
 }
---
+++ CampSleepaway.Test.Units/Application/NextOfKinTest.cs
@@ -0,0 +1,115 @@
+﻿using CampSleepaway.Application.NextOfKins;
+using CampSleepaway.Domain.Data;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampSleepaway.Test.Units.Application
+{
+    class NextOfKinTest : TestAddons
+    {
+        [Test]
+        public void AddNextOfKinByName_AllPropsNull_NoChange()
+        {
+            int expectedEntryChangesInDB = 0;
+
+            using var context = GetTestContext("AddNextOfKinBy");
+            var nextOfKinManager = new NextOfKinManager(context);
+            var nextOfKin = new NextOfKin();
+            int result = nextOfKinManager.AddNextOfKin(nextOfKin);
+
+            Assert.AreEqual(expectedEntryChangesInDB, result);
+        }
+
+        [Test]
+        public void AddNextOfKinByName_FirstNameNull_NoChange()
+        {
+            int expectedEntryChangesInDB = 0;
+
+            using var context = GetTestContext("AddNextOfKinBy");
+            var nextOfKinManager = new NextOfKinManager(context);
+            var nextOfKin = new NextOfKin()
+            {
+                LastName = "NextLastName"
+            };
+            var camper = new Camper()
+            {
+                FirstName = "CamperFirstName",
+                LastName = "CamperLastName",
+                DateOfBirth = new DateTime(2012, 01, 01)
+            };
+            nextOfKin.Children.Add(camper);
+            int result = nextOfKinManager.AddNextOfKin(nextOfKin);
+
+            Assert.AreEqual(expectedEntryChangesInDB, result);
+        }
+
+        [Test]
+        public void AddNextOfKinByName_LastNameNull_NoChange()
+        {
+            int expectedEntryChangesInDB = 0;
+
+            using var context = GetTestContext("AddNextOfKinBy");
+            var nextOfKinManager = new NextOfKinManager(context);
+            var nextOfKin = new NextOfKin()
+            {
+                FirstName = "NextFirstName"
+            };
+            var camper = new Camper()
+            {
+                FirstName = "CamperFirstName",
+                LastName = "CamperLastName",
+                DateOfBirth = new DateTime(2012, 01, 01)
+            };
+            nextOfKin.Children.Add(camper);
+            int result = nextOfKinManager.AddNextOfKin(nextOfKin);
+
+            Assert.AreEqual(expectedEntryChangesInDB, result);
+        }
+
+        [Test]
+        public void AddNextOfKinByName_NoChildren_NoChange()
+        {
+            int expectedEntryChangesInDB = 0;
+
+            using var context = GetTestContext("AddNextOfKinBy");
+            var nextOfKinManager = new NextOfKinManager(context);
+            var nextOfKin = new NextOfKin()
+            {
+                FirstName = "NextFirstName",
+                LastName = "NextLastName"
+            };
+            int result = nextOfKinManager.AddNextOfKin(nextOfKin);
+
+            Assert.AreEqual(expectedEntryChangesInDB, result);
+        }
+
+        [Test]
+        public void CanInsertCabinIntoDatabase()
+        {
+            int expectedEntryChangesInDB = 3;
+
+            using var context = GetTestContext("AddNextOfKinBy");
+
+            var nextOfKinManager = new NextOfKinManager(context);
+            var nextOfKin = new NextOfKin()
+            { 
+                FirstName = "NextFirstName",
+                LastName = "NextLastName"
+            };
+            var camper = new Camper()
+            {
+                FirstName = "CamperFirstName",
+                LastName = "CamperLastName",
+                DateOfBirth = new DateTime(2012, 01, 01)
+            };
+            nextOfKin.Children.Add(camper);
+            int result = nextOfKinManager.AddNextOfKin(nextOfKin);
+
+            Assert.AreEqual(expectedEntryChangesInDB, result);
+        }
+    }
+}
