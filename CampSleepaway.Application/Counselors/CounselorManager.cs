15e3c094c369644aa3422acb5b885f84fc6bdc27
Niklas Lindblad <niklas.o.lindblad@gmail.com>
2022-01-15 14:57:58 +0100

    added conselor creation

---
+++ CampSleepaway.Application/Counselors/CounselorManager.cs
@@ -0,0 +1,26 @@
+﻿using CampSleepaway.Domain.Data;
+using CampSleepaway.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampSleepaway.Application.Counselors
+{
+    public class CounselorManager : ManagerCore
+    {
+        public CounselorManager(CampSleepawayContext context) : base(context)
+        {
+        }
+
+        public int AddCounselor(Counselor counselor)
+        {
+            if (counselor.FirstName == null) { return 0; }
+            if (counselor.LastName == null) { return 0; }
+            _context.Counselors.Add(counselor);
+            var result = _context.SaveChanges();
+            return result;
+        }
+    }
+}
---
+++ CampSleepaway.Test.Units/Application/CounselorTest.cs
@@ -0,0 +1,85 @@
+﻿using CampSleepaway.Application.Counselors;
+using CampSleepaway.Domain.Data;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampSleepaway.Test.Units.Application
+{
+    class CounselorTest
+    {
+        [Test]
+        public void AddCounselor_NoProperties_NoChange()
+        {
+            int expectedChanges = 0;
+            using var context = TestAddons.GetTestContext("Counselor");
+            var manager = new CounselorManager(context);
+
+            Counselor counselor = new Counselor();
+            int result = manager.AddCounselor(counselor);
+
+            Assert.AreEqual(expectedChanges, result);
+        }
+
+        [Test]
+        public void AddCounselor_AllRequiredProperties_AddEntry()
+        {
+            int expectedChanges = 1;
+            using var context = TestAddons.GetTestContext("Counselor");
+            var manager = new CounselorManager(context);
+
+            Counselor counselor = new Counselor()
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                PhoneNumber = "PhoneNum",
+                Title = "Title"
+            };
+
+            int result = manager.AddCounselor(counselor);
+
+            Assert.AreEqual(expectedChanges, result);
+        }
+
+        [Test]
+        public void AddCounselor_MissingLastName_NoChange()
+        {
+            int expectedChanges = 0;
+            using var context = TestAddons.GetTestContext("Counselor");
+            var manager = new CounselorManager(context);
+
+            Counselor counselor = new Counselor()
+            {
+                FirstName = "FirstName",
+                PhoneNumber = "PhoneNum",
+                Title = "Title"
+            };
+
+            int result = manager.AddCounselor(counselor);
+
+            Assert.AreEqual(expectedChanges, result);
+        }
+
+        [Test]
+        public void AddCounselor_PhoneNumber_NoChange()
+        {
+            int expectedChanges = 0;
+            using var context = TestAddons.GetTestContext("Counselor");
+            var manager = new CounselorManager(context);
+
+            Counselor counselor = new Counselor()
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Title = "Title"
+            };
+
+            int result = manager.AddCounselor(counselor);
+
+            Assert.AreEqual(expectedChanges, result);
+        }
+    }
+}
