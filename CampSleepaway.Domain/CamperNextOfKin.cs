715a7b44bf379f8bcc02033722834e45e6fb5326
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-01-21 09:59:44 +0100

    added more tags for m-m relationship

--- CampSleepaway.Domain/CamperNextOfKin.cs
+++ CampSleepaway.Domain/CamperNextOfKin.cs
@@ -4,7 +4,9 @@ namespace CampSleepaway.Domain.Data
 {
     public class CamperNextOfKin
     {
+        [Key]
         public int NextOfKinId { get; set; }
+        [Key]
         public int CamperId { get; set; }
         [MaxLength(50)]
         public string NextOfKinRelationship { get; set; }
