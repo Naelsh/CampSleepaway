86ab09995c5c0820d7796873380ec955d941eb29
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2022-01-22 13:27:04 +0100

    updated seeddatacreation with proper managers and relationsships for counselor to cabin

--- CampSleepaway.Application/SeedData.cs
+++ CampSleepaway.Application/SeedData.cs
@@ -1,4 +1,7 @@
-﻿using CampSleepaway.Application.Campers;
+﻿using CampSleepaway.Application.Cabins;
+using CampSleepaway.Application.Campers;
+using CampSleepaway.Application.Counselors;
+using CampSleepaway.Application.NextOfKins;
 using CampSleepaway.Domain.Data;
 using CampSleepaway.Persistence;
 using System;
@@ -18,11 +21,54 @@ namespace CampSleepaway.Application
             CreateCampers(context);
             CreateCounselors(context);
             CreateNextOfKins(context);
-            context.SaveChanges();
+            AddCounserlorsToCabins(context);
+            AddCampersToCabin(context);
+        }
+
+        private static void CreateCabins(CampSleepawayContext context)
+        {
+            CabinManager cabinManager = new(context);
+            for (int cabinIndex = 0; cabinIndex < 3; cabinIndex++)
+            {
+                cabinManager.AddCabinByName($"Cabin name: {cabinIndex}");
+            }
+        }
+
+        private static void CreateCounselors(CampSleepawayContext context)
+        {
+            CounselorManager counselorManager = new(context);
+            for (int counselorIndex = 0; counselorIndex < 3; counselorIndex++)
+            {
+                var counselor = new Counselor()
+                {
+                    FirstName = $"Counselor-FN{counselorIndex}",
+                    LastName = $"Counselor-LN{counselorIndex}",
+                    Title = "Counselor",
+                    PhoneNumber = "012345678"
+                };
+                counselorManager.AddCounselor(counselor);
+            }
+        }
+
+        private static void CreateCampers(CampSleepawayContext context)
+        {
+            CamperManager camperManager = new(context);
+            for (int camperIndex = 0; camperIndex < 18; camperIndex++)
+            {
+                var newCamper = 
+                    new Camper()
+                    {
+                        FirstName = $"Camper-FN{camperIndex}",
+                        LastName = $"Camper-LN{camperIndex}",
+                        DateOfBirth = new DateTime(2012, Math.Clamp(camperIndex, 1, 12), camperIndex + 1)
+                    };
+                camperManager.AddCamper(newCamper);
+            }
         }
 
         private static void CreateNextOfKins(CampSleepawayContext context)
         {
+            var camperIds = context.Campers.Select(c => new { Id = c.Id });
             for (int nokIndex = 0; nokIndex < 5; nokIndex++)
             {
                 var newNextOfKin = new NextOfKin()
@@ -31,48 +77,59 @@ namespace CampSleepaway.Application
                     LastName = $"NextOfKin-LN{nokIndex}",
                     MailAddress = $"FN{nokIndex}.LN{nokIndex}@gmail.com"
                 };
-                newNextOfKin.Camper.Add(context.Campers.FirstOrDefault());
-                context.NextOfKins.Add(newNextOfKin);
+
+                newNextOfKin.Camper.Add(context.Campers.FirstOrDefault(c => c.Id == nokIndex + 1));
+
+                NextOfKinManager nextOfKinManager = new(context);
+                nextOfKinManager.AddNextOfKin(newNextOfKin, "Guardian");
+            }   
+        }
+
+        private static void AddCounserlorsToCabins(CampSleepawayContext context)
+        {
+            CabinManager cabinManager = new (context);
+            for (int counserlorIndex = 0; counserlorIndex < 3; counserlorIndex++)
+            {
+                DateTime startDate = new DateTime(2022,01,01);
+                DateTime endDate = new DateTime(2022,12,01);
+
+                cabinManager.AddCounselorToCabinById(
+                    counserlorIndex + 1,
+                    counserlorIndex + 1,
+                    startDate,
+                    endDate);
             }
         }
 
-        private static void CreateCabins(CampSleepawayContext context)
+        private static void SetRandomStartAndEndDate(out DateTime startDate, out DateTime endDate)
         {
-            for (int cabinIndex = 0; cabinIndex < 3; cabinIndex++)
+            Random random = new Random();
+            int startYear = random.Next(2021, 2022);
+            int startMonth = random.Next(1, 12);
+            int startDay = random.Next(1, 28);
+
+            int endYear = random.Next(2021, 2022);
+            int endMonth = random.Next(1, 12);
+            int endDay = random.Next(1, 28);
+
+            startDate = new DateTime(startYear, startMonth, startDay);
+            endDate = new DateTime(endYear, endMonth, endDay);
+
+            if (startDate.CompareTo(endDate) > 0)
             {
-                context.Cabins.Add(
-                    new Cabin()
-                    {
-                        Name = $"Cabin name: {cabinIndex}"
-                    });
+                DateTime temp = startDate;
+                startDate = endDate;
+                endDate = temp;
+            }
+            else if (startDate.CompareTo(endDate) == 0)
+            {
+                endDate.AddDays(1);
             }
         }
 
-        private static void CreateCounselors(CampSleepawayContext context)
+        private static void AddCampersToCabin(CampSleepawayContext context)
         {
-            for (int counselorIndex = 0; counselorIndex < 3; counselorIndex++)
-            {
-                var counselor = new Counselor()
-                {
-                    FirstName = $"Counselor-FN{counselorIndex}",
-                    LastName = $"Counselor-LN{counselorIndex}",
-                };
-                context.Counselors.Add(counselor);
-            }
-        }
-
-        private static void CreateCampers(CampSleepawayContext context)
-        {
-            for (int camperIndex = 0; camperIndex < 18; camperIndex++)
-            {
-                context.Campers.Add(
-                    new Camper()
-                    {
-                        FirstName = $"Camper-FN{camperIndex}",
-                        LastName = $"Camper-LN{camperIndex}",
-                        DateOfBirth = new DateTime(2012, Math.Clamp(camperIndex, 1, 12), camperIndex + 1)
-                    });
-            }
+            throw new NotImplementedException();
         }
     }
 }
